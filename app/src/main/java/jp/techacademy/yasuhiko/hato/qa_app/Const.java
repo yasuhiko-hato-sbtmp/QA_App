b2ee91f2a1aa372e0d1e9c3b6d8a30303f82e9ba
yasuhiko-hato-sbtmp <yasuhiko.hato.sbtmp@gmail.com>
2017-03-04 20:03:34 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yasuhiko/hato/qa_app/Const.java
@@ -0,0 +1,14 @@
+package jp.techacademy.yasuhiko.hato.qa_app;
+
+/**
+ * @author hatoy37
+ * @date 3/1/17
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
