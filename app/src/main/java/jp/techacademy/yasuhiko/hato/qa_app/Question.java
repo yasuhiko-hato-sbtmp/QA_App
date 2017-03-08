2e6b2694ec98f23c0ebb00c47129de9322ab1786
yasuhiko-hato-sbtmp <yasuhiko.hato.sbtmp@gmail.com>
2017-03-08 18:24:32 +0900

    Now on section 8

---
+++ app/src/main/java/jp/techacademy/yasuhiko/hato/qa_app/Question.java
@@ -0,0 +1,62 @@
+package jp.techacademy.yasuhiko.hato.qa_app;
+
+/**
+ * Created by hatoy37 on 3/4/17.
+ */
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Question implements Serializable {
+    private String mTitle;
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mQuestionUid;
+    private int mGenre;
+    private byte[] mBitmapArray;
+    private ArrayList<Answer> mAnswerArrayList;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getQuestionUid() {
+        return mQuestionUid;
+    }
+    public int getGenre() {
+        return mGenre;
+    }
+
+    public byte[] getImageBytes() {
+        return mBitmapArray;
+    }
+
+    public ArrayList<Answer> getAnswers() {
+        return mAnswerArrayList;
+    }
+
+    public Question(String title, String body, String name, String uid, String questionUid, int genre, byte[] bytes, ArrayList<Answer> answers) {
+        mTitle = title;
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mQuestionUid = questionUid;
+        mGenre = genre;
+        mBitmapArray = bytes.clone();
+        mAnswerArrayList = answers;
+    }
+}
