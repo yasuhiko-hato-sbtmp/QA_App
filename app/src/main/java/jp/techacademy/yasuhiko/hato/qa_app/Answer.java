9c2d085f8f22ac90c72362e9d7ac7658806694f7
yasuhiko-hato-sbtmp <yasuhiko.hato.sbtmp@gmail.com>
2017-03-08 18:24:06 +0900

    Now on section 8

---
+++ app/src/main/java/jp/techacademy/yasuhiko/hato/qa_app/Answer.java
@@ -0,0 +1,37 @@
+package jp.techacademy.yasuhiko.hato.qa_app;
+
+/**
+ * Created by hatoy37 on 3/4/17.
+ */
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getAnswerUid() {
+        return mAnswerUid;
+    }
+}
